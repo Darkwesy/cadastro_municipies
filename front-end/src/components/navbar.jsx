60ff2ad7b12582e32afd5db3961b2fe8e002885a
darkwesy <thiagopazba@gmail.com>
2024-05-19 07:16:34 -0300

    Ajustar elementos da navbar mobile

--- front-end/src/components/navbar.jsx
+++ front-end/src/components/navbar.jsx
@@ -4,20 +4,20 @@ import { TbBrandGithub } from 'react-icons/tb';
 
 export function Navbar() {
   return (
-    <div className="flex items-center justify-between sm:justify-between gap-4 max-h-8 px-6 sm:px-36">
-      <div className="flex items-center justify-center gap-4 sm:hover:shadow-md p-2 rounded-lg transition-all ease-in-out ">
-        <img src={logoCidade} alt="" className="h-10 cursor-pointer" />
+    <div className="flex items-center justify-between sm:justify-between sm:gap-4 max-h-8 px-6 sm:px-36">
+      <div className="flex items-center">
+        <img src={logoCidade} alt="" className="w-10 cursor-pointer" />
       </div>
-      <div className="flex sm:gap-4 items-center transition-all ease-in-out ">
-        <Button className="text-zinc-400 hover:text-zinc-500 rounded-lg bg-transparent hover:bg-transparent shadow-none sm:hover:shadow-md">
+      <div className="flex items-center sm:gap-4 transition-all ease-in-out">
+        <Button className="text-zinc-400 hover:text-zinc-500 rounded-lg bg-transparent hover:bg-transparent shadow-none sm:hover:shadow-md text-xs sm:text-base">
           Cadastrar Pessoa
         </Button>
-        <Button className="text-zinc-400 hover:text-zinc-500 rounded-lg bg-transparent hover:bg-transparent shadow-none sm:hover:shadow-md">
+        <Button className="text-zinc-400 hover:text-zinc-500 rounded-lg bg-transparent hover:bg-transparent shadow-none sm:hover:shadow-md text-xs sm:text-base">
           Base de dados
         </Button>
-        <Button className="text-zinc-400 hover:text-zinc-500 rounded-lg bg-transparent hover:bg-transparent shadow-none sm:hover:shadow-md">
+        <Button className="text-zinc-400 hover:text-zinc-500 rounded-lg bg-transparent hover:bg-transparent shadow-none sm:hover:shadow-md text-2xl">
           <a href="https://github.com/darkwesy">
-            <TbBrandGithub className="text-2xl" />
+            <TbBrandGithub />
           </a>
         </Button>
       </div>
